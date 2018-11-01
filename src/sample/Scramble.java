2af25a10f59a76c25e4beafe515ab536ecab9070
Makaron <1995@op.pl>
2018-11-01 19:22:52 +0100

    Improving the logic of generating scrambles and adding comments to the class

--- src/sample/Scramble.java
+++ src/sample/Scramble.java
@@ -2,10 +2,22 @@ package sample;
 
 import java.util.Random;
 
+/**
+ * A class that represents a scramble for Rubik's Cube
+ */
 public class Scramble {
 
     private String scramble = "";
-    public String[][] notationForScramble = new String [][]{
+
+    /*
+    A two-dimensional array that stores the notation of the algorithm. Its initialization is not accidental. Indexes of the first dimension of the array form 3 groups, representing 3 axes (three-dimensionality of the rubik's cube). The elements belonging to each axis result from division of the index of the first modulo dimension by 3.
+    As a result of:
+    The first axis -> array [0] and array [3]
+    The second axis -> array [1] and array [4]
+    The third axis -> array [2] and array [5]
+    We will need this to properly generate a scramble.
+     */
+    public final String[][] notationForScramble = new String [][]{
             { "U", "U2", "U'"},
             { "F", "F2", "F'"},
             { "R", "R2", "R'"},
@@ -21,20 +33,29 @@ public class Scramble {
         return scramble;
     }
 
+    /*
+    Method to generate new scramble.
+     */
     public void makeScramble(int scrambleLength){
         Random random = new Random();
         int i = 0, j = 0;
-        int lastIndex = 0, preLastIndex=0;
+        int lastIndex = 0, preLastIndex = 0;
         scramble = "";
 
         for (int x = 0; x < scrambleLength; x++){
-            while(i % 3 == lastIndex % 3) {
+
+            /*
+            Forcing a new index to be drawn, which is not the same
+            as the previous one or does not belong to the same axis as the penultimate one
+             */
+            while(i == lastIndex || i % 3 == preLastIndex % 3) {
                 i = random.nextInt(5);
             }
 
             j = random.nextInt(2);
+            preLastIndex = lastIndex;
             lastIndex = i;
-            scramble += notationForScramble[i][j] + " ";
+            scramble += notationForScramble[i][j] + " "; //Convert the generated numbers into scramble notation
         }
     }
 
